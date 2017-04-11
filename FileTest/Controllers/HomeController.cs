8502f20a18728d5dff33198a96f78b0de0514534
Mario Cacciatore <Mario Cacciatore>
2017-04-11 17:35:32 -0400

    Initial commit

---
+++ FileTest/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var vm = new ViewModel();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach(string file in Request.Files)
+                {
+                    var tempFile = Request.Files[file];
+                }
+                
+                ViewData.Add("Success", true);
+                return View();
+            }
+
+            return View(vm);
+        }
+    }
+
+    public class ViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<HttpPostedFileBase> Files { get; set; }
+    }
+}
